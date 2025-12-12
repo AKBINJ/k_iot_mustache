931dd1b111cc8a06d8035e59a058fe4da1d76b8f
AKBINJ <jyp10311@naver.com>
2025-12-12 17:45:56 +0900

    1212

---
+++ demo_ssr_v1_v1/src/main/java/org/example/demo_ssr_v1_v1/_core/errors/exception/Exception404.java
@@ -0,0 +1,11 @@
+package org.example.demo_ssr_v1_v1._core.errors.exception;
+
+/**
+ * 404 Not Found 인증 처리 오류
+ */
+public class Exception404 extends RuntimeException {
+
+    public Exception404(String msg) {
+        super(msg);
+    }
+}
