c3b47f9bc7c5db0068dcb8e04978492c284d28ec
AKBINJ <jyp10311@naver.com>
2025-12-29 09:03:53 +0900

    1229

---
+++ kor_spring_blog_ssr_v1/src/main/java/org/example/demo_ssr_v1_1/_core/errors/MyExceptionHandler.java
@@ -0,0 +1,130 @@
+package org.example.demo_ssr_v1_1._core.errors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.example.demo_ssr_v1_1._core.errors.exception.*;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// @ControllerAdvice - 모든 컨트롤러에서 발생하는 예외를 이 클래스에서 중앙 집중화 시킴
+// @RestControllerAdvice = @ControllerAdvice + @ResponseBody
+@ControllerAdvice
+@Slf4j
+public class MyExceptionHandler {
+
+    // 내가 지켜볼 예외를 명시를 해주면 ControllerAdvice 가 가지고와 처리 함
+    @ExceptionHandler(Exception400.class)
+    public String ex400(Exception400 e, HttpServletRequest request) {
+        log.warn("=== 400 에러 발생  ===");
+        log.warn("요청 URL : {}", request.getRequestURL());
+        log.warn("에러 메세지 : {}", e.getMessage());
+        log.warn("예외 클래스 : {}", e.getClass().getSimpleName());
+        request.setAttribute("msg", e.getMessage());
+        return "err/400";
+    }
+
+    @ExceptionHandler(Exception401.class)
+    @ResponseBody
+    public ResponseEntity<String> ex401(Exception401 e) {
+        String script = "<script>" +
+                "alert('" + e.getMessage() +"');" +
+                "location.href = '/login';" +
+                "</script>";
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .contentType(MediaType.TEXT_HTML)
+                .body(script);
+    }
+
+    @ExceptionHandler(Exception403.class)
+    @ResponseBody
+    public ResponseEntity<String> ex403(Exception403 e, HttpServletRequest request) {
+        String script = "<script>alert('"+e.getMessage()+"');" +
+                "history.back();" +
+                "</script>";
+
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .contentType(MediaType.TEXT_HTML)
+                .body(script);
+    }
+
+
+    // 404 인가 오류
+    // 템플릿 파일에서 세션 정보와 / Request 객체를 바로 접근 못하기 막았음 (기본값)
+    @ExceptionHandler(Exception404.class)
+    public String ex404(Exception404 e, HttpServletRequest request, Model model) {
+        log.warn("=== 404 에러 발생  ===");
+        log.warn("요청 URL : {}", request.getRequestURL());
+        log.warn("에러 메세지 : {}", e.getMessage());
+        log.warn("예외 클래스 : {}", e.getClass().getSimpleName());
+        //request.setAttribute("msg", e.getMessage());
+        model.addAttribute("msg", e.getMessage());
+        return "err/404";
+    }
+
+    // 500 서버 내부 오류
+    @ExceptionHandler(Exception500.class)
+    public String ex500(Exception500 e, HttpServletRequest request) {
+        log.warn("=== 500 에러 발생  ===");
+        log.warn("요청 URL : {}", request.getRequestURL());
+        log.warn("에러 메세지 : {}", e.getMessage());
+        log.warn("예외 클래스 : {}", e.getClass().getSimpleName());
+        request.setAttribute("msg", e.getMessage());
+        return "err/500";
+    }
+
+    // 데이터베이스 제약조건 위한 예외 처리
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public String handleDataViolationException(DataIntegrityViolationException e,
+                                               HttpServletRequest request,
+                                               Model model) {
+        log.warn("=== 데이터베이스 제약조건 위반 오류 발생  ===");
+        log.warn("요청 URL : {}", request.getRequestURL());
+        log.warn("에러 메세지 : {}", e.getMessage());
+        log.warn("예외 클래스 : {}", e.getClass().getSimpleName());
+        
+        // 외래키 제약 조건 위반인 경우 
+        String errorMessage = e.getMessage(); 
+        if(errorMessage != null && errorMessage.contains("FOREIGN KEY")) {
+            model.addAttribute("msg", "관련된 데이터가 있어 삭제할 수 없습니다.");
+        } else {
+            model.addAttribute("msg", "데이터베이스 제약 조건 위반");
+        }
+        return "err/500";
+    }
+
+    // 클래스 로딩 오류 처리 (NoClassDefFoundException, ClassNotFoundException 등)
+    @ExceptionHandler(Error.class)
+    public String handleError(Error e, HttpServletRequest request, Model model) {
+        log.warn("=== 예상하지 못한 에러 발생  ===");
+        log.warn("요청 URL : {}", request.getRequestURL());
+        log.warn("에러 메세지 : {}", e.getMessage());
+        log.warn("예외 클래스 : {}", e.getClass().getSimpleName());
+        model.addAttribute("msg", "심각한 오류 발생(클래스를 찾을 수 없습니다)");
+        return "err/500";
+    }
+
+
+    // 기타 모든 실행시점 오류 처리
+    @ExceptionHandler(RuntimeException.class)
+    public String handleRuntimeException(RuntimeException e,
+                                         HttpServletRequest request) {
+        log.warn("=== 예상하지 못한 에러 발생  ===");
+        log.warn("요청 URL : {}", request.getRequestURL());
+        log.warn("에러 메세지 : {}", e.getMessage());
+        log.warn("예외 클래스 : {}", e.getClass().getSimpleName());
+        request.setAttribute("msg", e.getMessage());
+        return "err/500";
+    }
+
+
+
+}
---
+++ kor_spring_blog_ssr_v1/src/main/java/org/example/demo_ssr_v1_1/user/UserController.java
@@ -0,0 +1,169 @@
+package org.example.demo_ssr_v1_1.user;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.example.demo_ssr_v1_1._core.errors.exception.Exception401;
+import org.example.demo_ssr_v1_1._core.errors.exception.Exception403;
+import org.example.demo_ssr_v1_1._core.errors.exception.Exception404;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+/**
+ *  사용자 Controller (표현 계층) 
+ *  핵심 개념 : 
+ *  - HTTP 요청을 받아서 처리 
+ *  - 요청 데이터 검증 및 파마리터 바인딩
+ *  - Service 레이어에 비즈니스 로직을 위힘
+ *  - 응답 데이터를 View 에 전달 함
+ *
+ */
+
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+
+    @GetMapping("/user/kakao")
+    public String kakaoCallback(@RequestParam(name = "code") String code, HttpSession session) {
+        try {
+            // 서비스단에 비즈니즈 로직을 위임 처림
+            User user = userService.카카오소셜로그인(code);
+            // 세션 정보게 사용자 정보 저장
+            session.setAttribute("sessionUser", user);
+            return "redirect:/";
+        } catch (Exception e) {
+            System.out.println("소셜 로그인 실패 " + e.getMessage());
+            // alert 창 (에러메세지) --> 로그인 페이지로 이동 처리
+            throw new Exception401(e.getMessage());
+        }
+    }
+
+    // 프로필 이미지 삭제 하기
+    @PostMapping("/user/profile-image/delete")
+    public String deleteProfileImage(HttpSession session) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        User updateUser = userService.프로필이미지삭제(sessionUser.getId());
+        // 왜 user 다시 받을까? -- 세션 정보가 (즉 프로필이 삭제 되었기 때문에)
+        // 세션 정보 갱신 처리 해주기 위함이다.
+        session.setAttribute("sessionUser", updateUser); // 세션 정보 갱신
+
+        // 일반적으로 POST 요청이 오면 PRG 패턴으로 설계 됨
+        // POST -> Redirect 처리 ---> Get 요청
+        return "redirect:/user/detail";
+    }
+
+    // 마이페이지
+    // http://localhost:8080/user/detail
+    @GetMapping("/user/detail")
+    public String detail(Model model, HttpSession session) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        User user = userService.마이페이지(sessionUser.getId());
+
+        model.addAttribute("user", user);
+        return "user/detail";
+    }
+
+
+    // 회원 정보 수정 화면 요청
+    // http://localhost:8080/user/update
+    @GetMapping("/user/update")
+    public String updateForm(Model model, HttpSession session) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        User user = userService.회원정보수정화면(sessionUser.getId());
+        model.addAttribute("user", user);
+
+        return "user/update-form";
+    }
+
+
+    // 회원 정수 수정 기능 요청 - 더티 체킹
+    // http://localhost:8080/user/update
+    @PostMapping("/user/update")
+    public String updateProc(UserRequest.UpdateDTO updateDTO, HttpSession session) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        try {
+            // 유효성 검사 (형식 검사)
+            updateDTO.validate();
+            User updateUser = userService.회원정보수정(updateDTO, sessionUser.getId());
+            // 회원 정보 수정은 - 세션 갱신해 주어야 한다.
+            session.setAttribute("sessionUser", updateUser);
+            return "redirect:/user/detail";
+        } catch (Exception e) {
+            return "user/update-form";
+        }
+    }
+
+
+
+    // 로그아웃 기능 요청
+    // http://localhost:8080/logout
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        // 세션 무효화
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    // 로그인 화면 요청
+    // http://localhost:8080/login
+    @GetMapping("/login")
+    public String loginForm() {
+        return "user/login-form";
+    }
+
+
+    // http://localhost:8080/login
+    @PostMapping("/login")
+    public String loginProc(UserRequest.LoginDTO loginDTO, HttpSession session) {
+        try {
+            // 유효성 검사
+            loginDTO.validate();
+            User sessionUser =  userService.로그인(loginDTO);
+            session.setAttribute("sessionUser", sessionUser);
+            return "redirect:/";
+        } catch (Exception e) {
+            // 로그인 실패시 다시 로그인 화면으로 처리
+            return "user/login-form";
+        }
+    }
+
+
+
+
+    // 회원가입 화면 요청
+    // http://localhost:8080/join
+    @GetMapping("/join")
+    public String joinFrom() {
+        return "user/join-form";
+    }
+
+    // 회원가입 기능 요청
+    // http://localhost:8080/join
+    @PostMapping("/join")
+    public String joinProc(UserRequest.JoinDTO joinDTO) {
+        joinDTO.validate();
+        userService.회원가입(joinDTO);
+        return "redirect:/login";
+    }
+
+}
